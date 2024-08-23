a734b942fcf873aad530a25ee87fbc44e143fb99
Luigi <Lmname999@gmail.com>
2024-08-23 23:22:58 +0100

    Pink Mouse eheh

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,13 @@ import { Analytics } from "@vercel/analytics/react";
 import { useContext } from 'react';
 import { LanguageContext } from './context/LanguageContext';
 
+import PinkMouse from './components/PinkMouse';
+
 const App = () => {
   const { language } = useContext(LanguageContext); // Obter idioma atual
 
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 min-h-screen relative">
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-pink-300 selection:text-cyan-1200 min-h-screen relative">
       <div className="fixed top-0 left-0 w-full h-full -z-10"></div>
       <div className="absolute inset-0 -z-10 min-h-screen w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
 
@@ -28,6 +30,7 @@ const App = () => {
         <FeedbackForm />
         <Contact />
         <Analytics />
+        <PinkMouse />
 
       </div>
     </div>
